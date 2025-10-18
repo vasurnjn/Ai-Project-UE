2f51c47cddb6610a4675254cf22516e2dcf99d70
DhruvsChopra <dhruvsinghchopra70@gmail.com>
2025-10-18 15:45:01 +0530

    Init Repo

---
+++ Source/DroneManagementSys/DroneManagementSys.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class DroneManagementSys : ModuleRules
+{
+	public DroneManagementSys(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"DroneManagementSys",
+			"DroneManagementSys/Variant_Platforming",
+			"DroneManagementSys/Variant_Combat",
+			"DroneManagementSys/Variant_Combat/AI",
+			"DroneManagementSys/Variant_SideScrolling",
+			"DroneManagementSys/Variant_SideScrolling/Gameplay",
+			"DroneManagementSys/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
